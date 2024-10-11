d50886fd72107dac4f5b0bf04f80430e19b9b8e5
daniehuuu <148598226+daniehuuu@users.noreply.github.com>
2024-10-10 21:34:31 -0500

    23

--- Week-9-Consultas/23.sql
+++ Week-9-Consultas/23.sql
@@ -1,13 +1,36 @@
 SELECT 
-    O.ShipCountry AS País,
-    MAX(OD.UnitPrice * OD.Quantity * (1 - OD.Discount)) AS Monto_Mayor
-FROM 
-    Orders O
-JOIN  
-    [Order Details] OD ON O.OrderID = OD.OrderID
-WHERE 
-    O.ShippedDate IS NOT NULL
-GROUP BY 
-    O.ShipCountry
-ORDER BY 
-    Monto_Mayor DESC;
+    País, 
+    ID, 
+    Total
+FROM (
+    SELECT 
+        O.ShipCountry AS País,
+        O.OrderID AS ID, 
+        SUM(OD.UnitPrice * OD.Quantity * (1 - OD.Discount)) AS Total
+    FROM 
+        Orders O
+    INNER JOIN  
+        [Order Details] OD ON O.OrderID = OD.OrderID
+    WHERE 
+        O.ShippedDate IS NOT NULL
+    GROUP BY 
+        O.ShipCountry, O.OrderID 
+) AS Totales
+WHERE Total = (
+    SELECT MAX(Total)
+    FROM (
+        SELECT 
+            O.ShipCountry AS País,
+            SUM(OD.UnitPrice * OD.Quantity * (1 - OD.Discount)) AS Total
+        FROM 
+            Orders O
+        INNER JOIN  
+            [Order Details] OD ON O.OrderID = OD.OrderID
+        WHERE 
+            O.ShippedDate IS NOT NULL
+        GROUP BY 
+            O.ShipCountry, O.OrderID
+    ) AS TotalesSub
+    WHERE TotalesSub.País = Totales.País
+)
+ORDER BY 1;
