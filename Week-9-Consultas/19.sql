83f56fec40eb36497d337d827fde4e5d48d49ba6
daniehuuu <148598226+daniehuuu@users.noreply.github.com>
2024-10-18 22:06:30 -0500

    19

---
+++ Week-9-Consultas/19.sql
@@ -0,0 +1,10 @@
+SELECT P.ProductID, P.ProductName as Nombre, C.CategoryID, P.UnitPrice as Precio
+FROM Products P
+INNER JOIN Categories C ON P.CategoryID = C.CategoryID
+WHERE P.UnitPrice > (
+    SELECT AVG(P2.UnitPrice) 
+    FROM Products P2 
+    INNER JOIN Categories C ON P.CategoryID = C.CategoryID
+    WHERE P.CategoryID = P2.CategoryID
+)
+ORDER BY 3, 4
