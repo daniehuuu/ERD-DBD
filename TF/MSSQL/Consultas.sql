08d59bbd49d788e38820be9132b6a2ad247ce559
daniehuuu <148598226+daniehuuu@users.noreply.github.com>
2024-10-24 10:36:12 -0500

    Registros de Historial laboral y 2 consultas

--- TF/MSSQL/Consultas.sql
+++ TF/MSSQL/Consultas.sql
@@ -51,3 +51,62 @@ SELECT FE.FAM_cod AS 'Código de Familiar', F.FAM_nom AS 'Nombre del Familiar', 
 FROM FAM_EMP FE
 JOIN Familiar F ON FE.FAM_cod = F.FAM_cod
 ORDER BY F.FAM_ape_p, F.FAM_nom
+
+--7.Muestra el cargo y salario actual, y el cargo y salario inmediato anterior de todos los empleados.
+SELECT 
+    E.EMP_cod AS Codigo_Empleado, 
+    E.EMP_nom AS Nombre, 
+    E.EMP_ape_p AS Apellido_Paterno, 
+    C.CAR_Desc AS Cargo_Actual, 
+    E.EMP_sld_bsc AS Salario_Base_Actual,
+
+    -- Subconsulta para obtener los datos del cargo anterior y fechas
+    HL2.CAR_Desc AS Cargo_Anterior,
+    HL2.HIS_sld_bsc AS Salario_Base_Anterior,
+    HL2.HIS_fec_in AS Fecha_Inicio_Anterior,
+    HL2.HIS_fec_fin AS Fecha_Cese_Anterior
+
+FROM Empleado E
+INNER JOIN Cargo C ON E.CAR_cod = C.CAR_cod
+LEFT JOIN (
+    SELECT HL2.EMP_cod, 
+           C2.CAR_Desc, 
+           HL2.HIS_sld_bsc, 
+           HL2.HIS_fec_in, 
+           HL2.HIS_fec_fin
+    FROM Historial_Laboral HL2
+    INNER JOIN Cargo C2 ON HL2.CAR_cod = C2.CAR_cod
+    WHERE HL2.HIS_fec_fin = (
+        SELECT MAX(HIS_fec_fin) 
+        FROM Historial_Laboral 
+        WHERE EMP_cod = HL2.EMP_cod
+    )
+) AS HL2 ON E.EMP_cod = HL2.EMP_cod
+ORDER BY Cargo_Anterior DESC
+
+--8.Muestra el primer cargo de cada empleado
+SELECT 
+    E.EMP_cod AS Codigo_Empleado, 
+    E.EMP_nom AS Nombre, 
+    E.EMP_ape_p AS Apellido_Paterno, 
+
+    -- Subconsulta para el primer cargo
+    CASE 
+        WHEN HL.HIS_fec_in IS NOT NULL THEN C2.CAR_Desc
+        ELSE C.CAR_Desc -- Si no existe historial, se toma el cargo actual
+    END AS Primer_Cargo
+
+FROM Empleado E
+INNER JOIN Cargo C ON E.CAR_cod = C.CAR_cod
+-- Se hace un LEFT JOIN para incluir el historial laboral si existe
+LEFT JOIN (
+    SELECT EMP_cod, HIS_fec_in, CAR_cod
+    FROM Historial_Laboral HL1
+    WHERE HIS_fec_in = (
+        SELECT MIN(HIS_fec_in)
+        FROM Historial_Laboral HL2
+        WHERE HL2.EMP_cod = HL1.EMP_cod
+    )
+) HL ON E.EMP_cod = HL.EMP_cod
+LEFT JOIN Cargo C2 ON HL.CAR_cod = C2.CAR_cod
+ORDER BY Primer_Cargo
